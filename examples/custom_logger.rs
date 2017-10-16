fdd248a9528d0ecd18146d9789407aeccc83cbec
Ashley Mannix <ashleymannix@live.com.au>
2017-10-17 08:45:12 +1000

    add examples for custom format and logger impls

---
+++ examples/custom_logger.rs
@@ -0,0 +1,67 @@
+/*!
+Using `env_logger` to drive a custom logger.
+
+Before running this example, try setting the `MY_LOG_LEVEL` environment variable to `info`:
+
+```no_run,shell
+$ export MY_LOG_LEVEL = 'info'
+```
+
+If you only want to change the way logs are formatted, look at the `custom_format` example.
+*/
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+use env_logger::Logger as EnvLogger;
+use log::{Log, Metadata, Record, SetLoggerError};
+
+struct MyLogger {
+    inner: EnvLogger
+}
+
+impl MyLogger {
+    fn new() -> MyLogger {
+        use env_logger::{Builder, Target};
+        let mut builder = Builder::new();
+        builder.target(Target::Silent);
+
+        if let Ok(ref filter) = std::env::var("MY_LOG_LEVEL") {
+           builder.parse(filter);
+        }
+
+        MyLogger {
+            inner: builder.build()
+        }
+    }
+
+    fn init() -> Result<(), SetLoggerError> {
+        log::set_boxed_logger(|max_level| {
+            let logger = Self::new();
+            max_level.set(logger.inner.filter());
+            Box::new(logger)
+        })
+    }
+}
+
+impl Log for MyLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        self.inner.enabled(metadata)
+    }
+
+    fn log(&self, record: &Record) {
+        if self.inner.matches(record) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {
+        self.inner.flush()
+    }
+}
+
+fn main() {
+    MyLogger::init().unwrap();
+
+    info!("a log from `MyLogger`");
+}
