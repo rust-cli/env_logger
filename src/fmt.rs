09135c8ba9c733df55157a2ee95dacb1214ba55a
Ashley Mannix <ashleymannix@live.com.au>
2017-12-22 14:42:39 +1000

    add examples and other fmt trait impls

--- src/fmt.rs
+++ src/fmt.rs
@@ -25,13 +25,77 @@ pub use termcolor::Color;
 /// `Formatter` implements the standard [`Write`] trait for writing log records.
 /// It also supports terminal colors, through the [`style`] method.
 /// 
+/// # Examples
+/// 
+/// Use the [`writeln`] macro to easily format a log record:
+/// 
+/// ```
+/// use std::io::Write;
+/// 
+/// let mut builder = env_logger::Builder::new();
+/// 
+/// builder.format(|buf, record| writeln!(buf, "{}: {}", record.level(), record.args()));
+/// ```
+/// 
 /// [`Write`]: https://doc.rust-lang.org/stable/std/io/trait.Write.html
+/// [`writeln`]: https://doc.rust-lang.org/stable/std/macro.writeln.html
 /// [`style`]: #method.style
 pub struct Formatter {
     buf: Rc<RefCell<Buffer>>,
 }
 
 /// A set of styles to apply to the terminal output.
+/// 
+/// Call [`Formatter.style`] to get a `Style` and use the builder methods to 
+/// set styling properties, like [color] and [weight].
+/// To print a value using the style, wrap it in a call to [`value`] when the log
+/// record is formatted.
+/// 
+/// # Examples
+/// 
+/// Create a bold, red colored style and use it to print the log level:
+/// 
+/// ```
+/// use std::io::Write;
+/// use env_logger::fmt::Color;
+/// 
+/// let mut builder = env_logger::Builder::new();
+/// 
+/// builder.format(|buf, record| {
+///     let mut level_style = buf.style();
+/// 
+///     level_style.set_color(Color::Red).set_bold(true);
+/// 
+///     writeln!(buf, "{}: {}",
+///         level_style.value(record.level()),
+///         record.args())
+/// });
+/// ```
+/// 
+/// Styles can be re-used to output multiple values:
+/// 
+/// ```
+/// use std::io::Write;
+/// use env_logger::fmt::Color;
+/// 
+/// let mut builder = env_logger::Builder::new();
+/// 
+/// builder.format(|buf, record| {
+///     let mut bold = buf.style();
+/// 
+///     bold.set_bold(true);
+/// 
+///     writeln!(buf, "{}: {} {}",
+///         bold.value(record.level()),
+///         bold.value("some bold text"),
+///         record.args())
+/// });
+/// ```
+/// 
+/// [`Formatter.style`]: struct.Formatter.html#method.style
+/// [color]: #method.color
+/// [weight]: #method.weight
+/// [`value`]: #method.value
 #[derive(Clone)]
 pub struct Style {
     buf: Rc<RefCell<Buffer>>,
@@ -39,25 +103,88 @@ pub struct Style {
 }
 
 /// A value that can be printed using the given styles.
+/// 
+/// It is the result of calling [`Style.value`].
+/// 
+/// [`Style.value`]: struct.Style.html#method.value
 pub struct StyledValue<'a, T> {
     style: &'a Style,
     value: T,
 }
 
 impl Style {
-    /// Set the foreground color.
+    /// Set the text color.
+    /// 
+    /// # Examples
+    /// 
+    /// Create a style with red text:
+    /// 
+    /// ```
+    /// use std::io::Write;
+    /// use env_logger::fmt::Color;
+    /// 
+    /// let mut builder = env_logger::Builder::new();
+    /// 
+    /// builder.format(|buf, record| {
+    ///     let mut style = buf.style();
+    /// 
+    ///     style.set_color(Color::Red);
+    /// 
+    ///     writeln!(buf, "{}", style.value(record.args()))
+    /// });
+    /// ```
     pub fn set_color(&mut self, color: Color) -> &mut Style {
         self.spec.set_fg(Some(color));
         self
     }
 
-    /// Make the text bold.
+    /// Set the text weight.
+    /// 
+    /// If `yes` is true then text will be written in bold.
+    /// If `yes` is false then text will be written in the default weight.
+    /// 
+    /// # Examples
+    /// 
+    /// Create a style with bold text:
+    /// 
+    /// ```
+    /// use std::io::Write;
+    /// 
+    /// let mut builder = env_logger::Builder::new();
+    /// 
+    /// builder.format(|buf, record| {
+    ///     let mut style = buf.style();
+    /// 
+    ///     style.set_bold(true);
+    /// 
+    ///     writeln!(buf, "{}", style.value(record.args()))
+    /// });
+    /// ```
     pub fn set_bold(&mut self, yes: bool) -> &mut Style {
         self.spec.set_bold(yes);
         self
     }
 
     /// Set the background color.
+    /// 
+    /// # Examples
+    /// 
+    /// Create a style with a yellow background:
+    /// 
+    /// ```
+    /// use std::io::Write;
+    /// use env_logger::fmt::Color;
+    /// 
+    /// let mut builder = env_logger::Builder::new();
+    /// 
+    /// builder.format(|buf, record| {
+    ///     let mut style = buf.style();
+    /// 
+    ///     style.set_bg(Color::Yellow);
+    /// 
+    ///     writeln!(buf, "{}", style.value(record.args()))
+    /// });
+    /// ```
     pub fn set_bg(&mut self, color: Color) -> &mut Style {
         self.spec.set_bg(Some(color));
         self
@@ -66,6 +193,27 @@ impl Style {
     /// Wrap a value in the style.
     /// 
     /// The same `Style` can be used to print multiple different values.
+    /// 
+    /// # Examples
+    /// 
+    /// Create a bold, red colored style and use it to print the log level:
+    /// 
+    /// ```
+    /// use std::io::Write;
+    /// use env_logger::fmt::Color;
+    /// 
+    /// let mut builder = env_logger::Builder::new();
+    /// 
+    /// builder.format(|buf, record| {
+    ///     let mut style = buf.style();
+    /// 
+    ///     style.set_color(Color::Red).set_bold(true);
+    /// 
+    ///     writeln!(buf, "{}: {}",
+    ///         style.value(record.level()),
+    ///         record.args())
+    /// });
+    /// ```
     pub fn value<T>(&self, value: T) -> StyledValue<T> {
         StyledValue {
             style: &self,
@@ -74,10 +222,11 @@ impl Style {
     }
 }
 
-/// An RFC3339 formatted timestamp.
+/// An [RFC3339] formatted timestamp.
 /// 
 /// The timestamp implements [`Display`] and can be written to a [`Formatter`].
 /// 
+/// [RFC3339]: https://www.ietf.org/rfc/rfc3339.txt
 /// [`Display`]: https://doc.rust-lang.org/stable/std/fmt/trait.Display.html
 /// [`Formatter`]: struct.Formatter.html
 pub struct Timestamp(DateTime<Utc>);
@@ -89,7 +238,30 @@ impl Formatter {
         }
     }
 
-    /// Begin a new style.
+    /// Begin a new [`Style`].
+    /// 
+    /// # Examples
+    /// 
+    /// Create a bold, red colored style and use it to print the log level:
+    /// 
+    /// ```
+    /// use std::io::Write;
+    /// use env_logger::fmt::Color;
+    /// 
+    /// let mut builder = env_logger::Builder::new();
+    /// 
+    /// builder.format(|buf, record| {
+    ///     let mut level_style = buf.style();
+    /// 
+    ///     level_style.set_color(Color::Red).set_bold(true);
+    /// 
+    ///     writeln!(buf, "{}: {}",
+    ///         level_style.value(record.level()),
+    ///         record.args())
+    /// });
+    /// ```
+    /// 
+    /// [`Style`]: struct.Style.html
     pub fn style(&self) -> Style {
         Style {
             buf: self.buf.clone(),
@@ -97,7 +269,25 @@ impl Formatter {
         }
     }
 
-    /// Get a timestamp.
+    /// Get a [`Timestamp`] for the current date and time in UTC.
+    /// 
+    /// # Examples
+    /// 
+    /// Include the current timestamp with the log record:
+    /// 
+    /// ```
+    /// use std::io::Write;
+    /// 
+    /// let mut builder = env_logger::Builder::new();
+    /// 
+    /// builder.format(|buf, record| {
+    ///     let ts = buf.timestamp();
+    /// 
+    ///     writeln!(buf, "{}: {}: {}", ts, record.level(), record.args())
+    /// });
+    /// ```
+    /// 
+    /// [`Timestamp`]: struct.Timestamp.html
     pub fn timestamp(&self) -> Timestamp {
         Timestamp(Utc::now())
     }
@@ -165,19 +355,28 @@ impl fmt::Debug for Style {
     }
 }
 
-impl<'a, T: fmt::Debug> fmt::Debug for StyledValue<'a, T> {
-    fn fmt(&self, f: &mut fmt::Formatter)->fmt::Result {
-        self.write_fmt(|| T::fmt(&self.value, f))
-    }
+macro_rules! impl_styled_value_fmt {
+    ($($fmt_trait:path),*) => {
+        $(
+            impl<'a, T: $fmt_trait> $fmt_trait for StyledValue<'a, T> {
+                fn fmt(&self, f: &mut fmt::Formatter)->fmt::Result {
+                    self.write_fmt(|| T::fmt(&self.value, f))
+                }
+            }
+        )*
+    };
 }
 
-impl<'a, T: fmt::Display> fmt::Display for StyledValue<'a, T> {
-    fn fmt(&self, f: &mut fmt::Formatter)->fmt::Result {
-        self.write_fmt(|| T::fmt(&self.value, f))
-    }
-}
-
-// TODO: Other `fmt` traits
+impl_styled_value_fmt!(
+    fmt::Debug,
+    fmt::Display,
+    fmt::Pointer,
+    fmt::Octal,
+    fmt::Binary,
+    fmt::UpperHex,
+    fmt::LowerHex,
+    fmt::UpperExp,
+    fmt::LowerExp);
 
 impl fmt::Display for Timestamp {
     fn fmt(&self, f: &mut fmt::Formatter)->fmt::Result {
