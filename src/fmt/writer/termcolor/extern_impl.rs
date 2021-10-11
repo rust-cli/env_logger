a08e0227e707f82a14a8b3b40518bb3d48660439
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-10-11 20:20:49 +0200

    Add `Style::set_dimmed` to support dimmed text

--- src/fmt/writer/termcolor/extern_impl.rs
+++ src/fmt/writer/termcolor/extern_impl.rs
@@ -339,6 +339,33 @@ impl Style {
         self
     }
 
+    /// Set whether the text is dimmed.
+    ///
+    /// If `yes` is true then text will be written in a dimmer color.
+    /// If `yes` is false then text will be written in the default color.
+    ///
+    /// # Examples
+    ///
+    /// Create a style with dimmed text:
+    ///
+    /// ```
+    /// use std::io::Write;
+    ///
+    /// let mut builder = env_logger::Builder::new();
+    ///
+    /// builder.format(|buf, record| {
+    ///     let mut style = buf.style();
+    ///
+    ///     style.set_dimmed(true);
+    ///
+    ///     writeln!(buf, "{}", style.value(record.args()))
+    /// });
+    /// ```
+    pub fn set_dimmed(&mut self, yes: bool) -> &mut Style {
+        self.spec.set_dimmed(yes);
+        self
+    }
+
     /// Set the background color.
     ///
     /// # Examples
